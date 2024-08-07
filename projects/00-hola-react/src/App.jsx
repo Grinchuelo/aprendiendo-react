14fe1572dd83d7096ff4be107b2a7dbf294dbc93
Grin <santinogreen14@gmail.com>
2024-08-07 20:50:20 -0300

    Primera version de 00-hola-react sin terminar

---
+++ projects/00-hola-react/src/App.jsx
@@ -0,0 +1,17 @@
+export function App () {
+    return (
+        <article>
+            <header>
+                <img src="https://avatars.githubusercontent.com/u/111719839?v=4" alt="Foto de perfil" />
+                <div>
+                    <strong>Santino Green</strong>
+                    <span>@grinchueloo</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
