40ce05a85ffe9eacaa457686c005da76880f5676
Grin <santinogreen14@gmail.com>
2024-08-10 21:45:20 -0300

    00-hola-react 100%

--- projects/00-hola-react/src/App.jsx
+++ projects/00-hola-react/src/App.jsx
@@ -1,15 +1,49 @@
 import './App.css';
 import './index.css'
 import { TwFollowCard } from './twFollowCard';
+import { useState } from "react";
+
+const users = [
+    {
+        username: 'Grinchuelo',
+        name: 'Santino Green',
+        isFollowing: true
+    },
+    {
+        username: 'Manu',
+        name: 'Manuel Medina',
+        isFollowing: true
+    },
+    {
+        username: 'Larsids',
+        name: 'Lara Fernandez',
+        isFollowing: false
+    },
+    {
+        username: 'Neriii',
+        name: 'Merlina Addams',
+        isFollowing: false
+    }
+]
 
 export function App () {
+    
+    const [username, setUsername] = useState('grinchuelo')
     const formatUsername = (username) => `@${username}`;
+    console.log('render with username: ', username)
     return (
         <section className='App'>
-            <TwFollowCard username="grinchuelo" formatUsername={formatUsername} isFollowing> Santino Green </TwFollowCard>
+            {users.map(user => {
+                const {username,  name, isFollowing} = user;
+                return (
+                    <TwFollowCard key={username} username={username} name={name} isFollowing={isFollowing}></TwFollowCard>
+                )
+                
+            })}
+            {/* <TwFollowCard username={username} formatUsername={formatUsername} initialIsFollowing={false}> Santino Green </TwFollowCard>
 
-            <TwFollowCard name="Elon Musk" username="elonmusk" formatUsername={formatUsername} isFollowing={false}/>
-            {/*<TwFollowCard name="Miguel Ángel Durán" username="midudev" formatUsername={formatUsername} isFollowing={false}/> */}
+            <TwFollowCard name="Elon Musk" username="elonmusk" formatUsername={formatUsername} initialIsFollowing> Elon Musk </TwFollowCard> */}
+            
         </section>
     )
 }
--- projects/00-hola-react/src/twFollowCard.jsx
+++ projects/00-hola-react/src/twFollowCard.jsx
@@ -1,25 +1,34 @@
-export function TwFollowCard ({children, username, isFollowing}) {
+import { useState } from "react";
+
+export function TwFollowCard ({name, username, initialIsFollowing}) {
     const avatar = `https://unavatar.io/github/${username}`;
     const addAt = (username) => `@${username}`;
+    const [isFollowing, setisFollowing] = useState(initialIsFollowing);
+
     let btnIsFollowing = isFollowing ? 'Siguiendo' : 'Seguir';
-    if (btnIsFollowing == 'Siguiendo') {
-        
-    }
     const buttonClassName = isFollowing
         ? 'tw-followCard-button is-following'
-        : 'tw-followCard-button'
+        : 'tw-followCard-button';
+
+
+    const handleClick = () => {
+        setisFollowing(!isFollowing);
+    }
     return (
         <article className="tw-followCard">
             <header className="tw-followCard-header">
                 <img className="tw-followCard-header-avatar" src={avatar} alt="Foto de perfil" />
                 <div className="tw-followCard-info">
-                    <strong>{children}</strong>
+                    <strong>{name}</strong>
                     <span className="tw-followCard-infouserName">{addAt(username)}</span>
                 </div>
             </header>
 
             <aside>
-                <button className={buttonClassName}>{btnIsFollowing}</button>
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className="tw-followCard-btnText">{btnIsFollowing}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+                </button>
             </aside>
         </article>
     );
